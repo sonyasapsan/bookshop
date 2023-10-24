bd8ed55f3bb1064a11774b3c5eab890e60da7db6
Sonia Baranova <sofiasapsan@gmail.com>
2023-10-24 11:06:24 +0300

    jpa

--- src/main/java/com/example/bookshop/repository/BookRepository.java
+++ src/main/java/com/example/bookshop/repository/BookRepository.java
@@ -1,7 +1,6 @@
 package com.example.bookshop.repository;
 
 import com.example.bookshop.model.Book;
-import jakarta.persistence.EntityManagerFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
